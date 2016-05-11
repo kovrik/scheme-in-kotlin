dc45a9d5aa0d147a8b2f83b4212a05e5b0fc0759
kovrik <kovrik0@gmail.com>
2016-05-11 18:26:10 +1200

    evlis and evmeta

--- src/main/java/main/core/evaluator/Evaluator.java
+++ src/main/java/main/core/evaluator/Evaluator.java
@@ -2,11 +2,11 @@ package main.core.evaluator;
 
 import main.core.ast.SCMList;
 import main.core.ast.SCMSymbol;
+import main.core.environment.Environment;
+import main.core.environment.IEnvironment;
 import main.core.procedures.IFn;
 import main.core.procedures.Procedure;
 import main.core.specialforms.SpecialForm;
-import main.core.environment.Environment;
-import main.core.environment.IEnvironment;
 
 import java.util.HashMap;
 import java.util.List;
@@ -19,60 +19,85 @@ public class Evaluator implements IEvaluator {
 
     if (sexp instanceof SCMSymbol) {
       /* Symbol */
-      return env.find(sexp);
-    } else if (sexp instanceof String && ((String) sexp).startsWith(",")) {
-      /* Meta */
-      if (",q".equals(sexp)) {
-        System.out.println("Bye!");
-        System.exit(0);
+      if (((SCMSymbol) sexp).getValue().startsWith(",")) {
+        /* Meta */
+        return evmeta(((SCMSymbol)sexp).getValue());
       }
+      return env.find(sexp);
     } else if (!(sexp instanceof List)) {
       return sexp;
     } else if (sexp instanceof SCMList) {
+      return evlis(sexp, env);
+    }
+    throw new IllegalArgumentException("Evaluation error: " + sexp);
+  }
 
-      SCMList<Object> list = (SCMList<Object>)sexp;
-      if (list.isEmpty()) {
-        throw new IllegalArgumentException("Unexpected syntax in form " + list);
-      }
-      Object op = list.getFirst();
+  /**
+   * Gets a procedure `fn`, parameters `args`, environment `env`,
+   * invokes the procedure and returns a result.
+   */
+  public Object apply(Object fn, Object[] args, IEnvironment env) {
 
-      /* Special Form */
-      if (op instanceof SpecialForm) {
-        return ((SpecialForm)op).eval(list, env, this);
-      }
-      /* Function */
-      Object fn = eval(op, env);
-      if (fn == null) {
-        throw new UnsupportedOperationException("Unbound variable: " + op);
-      }
-      /* Evaluate arguments */
-      Object[] args = new Object[list.size() - 1];
-      for (int i = 1; i < list.size(); i++) {
-        args[i - 1] = eval(list.get(i), env);
-      }
-      /* Procedure */
-      // FIXME Make generic as IFn?
-      if (fn instanceof Procedure) {
-        Procedure procedure = (Procedure) fn;
-        List<Object> params = procedure.getParams();
-        if (args.length != params.size()) {
-          throw new IllegalArgumentException("Wrong number of arguments: expected " + params.size() +
-              ", actual " + args.length);
-        }
-        Map<Object, Object> values = new HashMap<Object, Object>(params.size());
-        for (int i = 0; i < params.size(); i++) {
-          values.put(params.get(i), args[i]);
-        }
-        return eval(procedure.getBody(), new Environment(values, env));
-      }
+    Procedure procedure = (Procedure) fn;
+    List<Object> params = procedure.getParams();
+    if (args.length != params.size()) {
+      throw new IllegalArgumentException("Wrong number of arguments: expected " + params.size() +
+          ", actual " + args.length);
+    }
+    Map<Object, Object> values = new HashMap<Object, Object>(params.size());
+    for (int i = 0; i < params.size(); i++) {
+      values.put(params.get(i), args[i]);
+    }
+    return eval(procedure.getBody(), new Environment(values, env));
+  }
 
-      try {
-        return ((IFn)fn).invoke(args);
-      } catch (ExecutionException e) {
-        e.printStackTrace();
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
+  /**
+   * Evaluate a list
+   */
+  public Object evlis(Object sexp, IEnvironment env) {
+
+    SCMList<Object> list = (SCMList<Object>)sexp;
+    if (list.isEmpty()) {
+      throw new IllegalArgumentException("Unexpected syntax in form " + list);
+    }
+    Object op = list.getFirst();
+
+    /* Special Form */
+    if (op instanceof SpecialForm) {
+      return ((SpecialForm)op).eval(list, env, this);
+    }
+    /* Function */
+    Object fn = eval(op, env);
+    if (fn == null) {
+      throw new UnsupportedOperationException("Unbound variable: " + op);
+    }
+    /* Evaluate arguments */
+    Object[] args = new Object[list.size() - 1];
+    for (int i = 1; i < list.size(); i++) {
+      args[i - 1] = eval(list.get(i), env);
+    }
+    /* Procedure */
+    // FIXME Make generic as IFn?
+    if (fn instanceof Procedure) {
+      return apply(fn, args, env);
+    }
+    try {
+      return ((IFn)fn).invoke(args);
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+    throw new IllegalArgumentException("Evaluation error: " + sexp);
+  }
+
+  /**
+   * Evaluate meta
+   */
+  public Object evmeta(String sexp) {
+    if (",q".equals(sexp)) {
+      System.out.println("Bye!");
+      System.exit(0);
     }
     throw new IllegalArgumentException("Evaluation error: " + sexp);
   }
