229cec1f2e09f0463a0b600ad1a224ab41337984
kovrik <kovrik0@gmail.com>
2016-07-02 20:22:36 +1200

    Unmodifiable NIL prototype

--- src/main/java/core/scm/SCMList.java
+++ src/main/java/core/scm/SCMList.java
@@ -1,11 +1,37 @@
 package core.scm;
 
 import java.util.*;
+import java.util.function.UnaryOperator;
 
 public class SCMList<E> extends LinkedList<E> {
 
   // TODO Make it a Singleton
-  public static final SCMList NIL = new SCMList();
+  public static final SCMList NIL = new SCMList<Object>() {
+    @Override
+    public Object set(int index, Object element) {
+      throw new UnsupportedOperationException();
+    }
+    @Override
+    public void add(int index, Object element) {
+      throw new UnsupportedOperationException();
+    }
+    @Override
+    public Object remove(int index) {
+      throw new UnsupportedOperationException();
+    }
+    // FIXME Why not working?
+//    @Override
+//    public boolean addAll(int index, Collection c) {
+//      throw new UnsupportedOperationException();
+//    }
+    public void replaceAll(UnaryOperator operator) {
+      throw new UnsupportedOperationException();
+    }
+    public void sort(Comparator c) {
+      throw new UnsupportedOperationException();
+    }
+    // TODO @Override ListIterator?
+  };
 
   public SCMList(Collection<? extends E> c) {
     super(c);
