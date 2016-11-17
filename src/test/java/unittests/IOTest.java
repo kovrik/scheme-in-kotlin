50e96d7d411a3df97a69dada89b7ae986bea2884
kovrik <kovrik0@gmail.com>
2016-11-17 19:26:02 +1300

    More IOTests

--- src/test/java/unittests/IOTest.java
+++ src/test/java/unittests/IOTest.java
@@ -1,12 +1,15 @@
 package unittests;
 
 import core.Main;
+import core.scm.SCMEof;
 import core.scm.SCMInputPort;
 import core.scm.SCMOutputPort;
 import org.junit.Test;
 
 import java.io.*;
 
+import static core.scm.SCMBoolean.FALSE;
+import static core.scm.SCMBoolean.TRUE;
 import static org.junit.Assert.assertEquals;
 
 public class IOTest extends AbstractTest {
@@ -28,4 +31,26 @@ public class IOTest extends AbstractTest {
     assertEquals(new SCMInputPort(inputStream), eval("(current-input-port)", env));
     Main.setCurrentInputPort(currentInputPort);
   }
+
+  @Test
+  public void testEofObject() {
+    assertEquals(SCMEof.EOF, eval("eof", env));
+    assertEquals(TRUE, eval("(eof-object? eof)", env));
+    assertEquals(FALSE, eval("(eof-object? 0)", env));
+    assertEquals(FALSE, eval("(eof-object? \"test\")", env));
+  }
+
+  @Test
+  public void testPortPredicates() {
+    assertEquals(TRUE, eval("(port? (current-input-port))", env));
+    assertEquals(TRUE, eval("(port? (current-output-port))", env));
+    assertEquals(TRUE, eval("(input-port? (current-input-port))", env));
+    assertEquals(TRUE, eval("(output-port? (current-output-port))", env));
+
+    assertEquals(FALSE, eval("(port? 1)", env));
+    assertEquals(FALSE, eval("(output-port? (current-input-port))", env));
+    assertEquals(FALSE, eval("(input-port? (current-output-port))", env));
+  }
+
+  // TODO close-*-port, write-char, peek-char, read-char, read, write
 }
