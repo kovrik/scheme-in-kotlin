49601ba05f5b0e43bb20a5b964daa0d2d6a43ed0
kovrik <kovrik0@gmail.com>
2016-12-15 21:13:13 +1300

    FIx atan for Complex numbers

--- src/main/java/core/procedures/math/trigonometry/Atan.java
+++ src/main/java/core/procedures/math/trigonometry/Atan.java
@@ -63,17 +63,33 @@ public class Atan extends AFn {
 
     double a2 = a*a;
     double b2 = b*b;
-    // FIXME
-//    double re = Math.atan((2*a)/(1 - a2 - b2))/2 ;
-//    if (Double.isInfinite(re) || Double.isNaN(re)) {
-//      return Double.NaN;
-//    }
-//
-//    double im = (Math.log((a2 + (1+b)*(1+b)) / (a2 + (1-b)*(1-b)) ))/4;
-//    if (Double.isInfinite(im) || Double.isNaN(im)) {
-//      return Double.NaN;
-//    }
-//    return new SCMBigComplex(re, -im);
-    throw new UnsupportedOperationException("Not implemented yet!");
+    double re;
+    if (r.compareTo(BigDecimal.ZERO) == 0 && (i.compareTo(BigDecimal.ONE.negate()) > 0) && (i.compareTo(BigDecimal.ONE) < 0)) {
+      /* when x = 0 and -1 < y < 1 */
+      re = 0;
+    } else if (r.compareTo(BigDecimal.ZERO) == 0 && i.multiply(i).compareTo(BigDecimal.ONE) > 0) {
+      /* when x = 0 and 1 < y^2
+       * re(arctan(x + iy)) = pi/2 */
+      re = Math.PI / 2;
+    } else if (r.compareTo(BigDecimal.ZERO) > 0) {
+      /* when x > 0
+       * re(arctan(x + iy)) = pi/4 - (1/2) arctan ( (1 - x^2 - y^2)/(2x) ) */
+      re = Math.PI/4 - 0.5*Math.atan((1 - a2 - b2) / (2*a));
+    } else {
+      /* when x < 0
+       * re(arctan(x + iy)) = -pi/4 - (1/2) arctan ( (1 - x^2 - y^2)/(2x) ) */
+      re = -Math.PI/4 - 0.5*Math.atan((1 - a2 - b2) / (2*a));
+    }
+
+    if (Double.isInfinite(re) || Double.isNaN(re)) {
+      return Double.NaN;
+    }
+
+    /* im(arctan(x + iy)) = -(1/4) ln ((1 - x^2 - y^2)^2 + (2x)^2) + (1/2) ln ((1 + y)^2 + x^2) */
+    double im = -0.25*Math.log((1 - a2 - b2)*(1 - a2 - b2) + (4*a2)) + 0.5*Math.log((1 + b)*(1 + b) + a2);
+    if (Double.isInfinite(im) || Double.isNaN(im)) {
+      return Double.NaN;
+    }
+    return new SCMBigComplex(re, im);
   }
 }
