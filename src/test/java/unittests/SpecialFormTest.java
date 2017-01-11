5e3afa5c765c3ff6cd1ab08ad67a5f81ae11bd73
kovrik <kovrik0@gmail.com>
2017-01-11 20:49:26 +1300

    Additional test for variadic lambdas

--- src/test/java/unittests/SpecialFormTest.java
+++ src/test/java/unittests/SpecialFormTest.java
@@ -198,6 +198,8 @@ public class SpecialFormTest extends AbstractTest {
     eval(d2, env);
     assertEquals(9L, eval("(test-internal-define2)", env));
 
+    assertEquals(list(3L, 4L, 5L), eval("((lambda (a b c . d) d) 0 1 2 3 4 5)", env));
+
     // TODO check that define is a top form in a body
 //    String d3 = "(let ((a 1)) a (define a 5) a)";
 //    try {
