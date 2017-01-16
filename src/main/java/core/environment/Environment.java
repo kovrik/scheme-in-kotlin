1c400a2c15619987844f4e6dd1595bcb6f30f516
kovrik <kovrik0@gmail.com>
2017-01-16 16:28:32 +1300

    Make Environment extend HashMap instead of wrapping it

--- src/main/java/core/environment/Environment.java
+++ src/main/java/core/environment/Environment.java
@@ -5,22 +5,12 @@ import core.scm.SCMClass;
 
 import java.util.*;
 
-public class Environment implements ISCMClass {
-
-  private Map<Object, Object> context = new HashMap<>();
+public class Environment extends HashMap<Object, Object> implements ISCMClass {
 
   private Environment outer = null;
 
   public Environment(Environment outer) {
     this.outer = outer;
-  }
-
-  public Object get(Object key) {
-    return context.get(key);
-  }
-
-  public boolean containsKey(Object key) {
-    return context.containsKey(key);
   }
 
   public List<String> getLibraryProcedures() {
@@ -29,7 +19,7 @@ public class Environment implements ISCMClass {
   }
 
   public Object find(Object key) {
-    Object value = context.get(key);
+    Object value = get(key);
     if (value == null) {
       if (outer == null) {
         throw new IllegalArgumentException("Unbound variable: " + key);
@@ -40,7 +30,7 @@ public class Environment implements ISCMClass {
   }
 
   public Object findOrNull(Object key) {
-    Object value = context.get(key);
+    Object value = get(key);
     if (value == null) {
       if (outer == null) {
         return null;
@@ -51,27 +41,19 @@ public class Environment implements ISCMClass {
   }
 
   public Object findAndPut(Object key, Object value) {
-    Object v = context.get(key);
+    Object v = get(key);
     if (v == null) {
       if (outer == null) {
         throw new IllegalArgumentException("Unbound variable: " + key);
       }
       return outer.findAndPut(key, value);
     }
-    return context.put(key, value);
-  }
-
-  public Object put(Object key, Object value) {
-    return context.put(key, value);
+    return put(key, value);
   }
 
   @Override
   public SCMClass getSCMClass() {
     return SCMClass.ENVIRONMENT;
-  }
-
-  public Environment getOuter() {
-    return outer;
   }
 
   @Override
