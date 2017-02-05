309c527a173e697697bcea6690e8b4791d7a7424
kovrik <kovrik0@gmail.com>
2017-02-05 20:14:11 +1300

    Simplify 'sqrt' procedure

--- src/main/java/core/procedures/math/Sqrt.java
+++ src/main/java/core/procedures/math/Sqrt.java
@@ -3,7 +3,6 @@ package core.procedures.math;
 import core.procedures.AFn;
 import core.scm.FnArgs;
 import core.scm.SCMBigComplex;
-import core.utils.BigDecimalMath;
 
 import java.math.BigDecimal;
 
@@ -26,26 +25,12 @@ public final class Sqrt extends AFn {
   }
 
   public static Number sqrt(Number number) {
-    if (number instanceof Long) {
-      return Math.sqrt((Long) number);
-    } else if (number instanceof Double) {
-      return Math.sqrt((Double) number);
-    } else if (number instanceof BigDecimal) {
-      if (Double.isInfinite(number.doubleValue())) {
-        return Double.POSITIVE_INFINITY;
-      }
-      if (Double.isFinite(number.doubleValue())) {
-        return Math.sqrt(number.doubleValue());
-      }
-      // FIXME Loss of precision
-      return BigDecimalMath.sqrt((BigDecimal) number);
-    } else if (number instanceof SCMBigComplex) {
+    if (number instanceof SCMBigComplex) {
       if (((SCMBigComplex)number).getIm().compareTo(BigDecimal.ZERO) == 0) {
         return sqrt(((SCMBigComplex)number).getRe());
       }
       return ((SCMBigComplex)number).sqrt();
-    } else {
-      return Math.sqrt(number.doubleValue());
     }
+    return Math.sqrt(number.doubleValue());
   }
 }
