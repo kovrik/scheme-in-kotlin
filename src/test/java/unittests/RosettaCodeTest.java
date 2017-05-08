3526dbebaf6a1b0a2f2e98425754d1643ee77ba5
kovrik <kovrik0@gmail.com>
2017-05-08 21:28:54 +1200

    Haversine formula test

--- src/test/java/unittests/RosettaCodeTest.java
+++ src/test/java/unittests/RosettaCodeTest.java
@@ -329,4 +329,19 @@ public class RosettaCodeTest extends AbstractTest {
     String code = "(A 10 (lambda () 1) (lambda () -1) (lambda () -1) (lambda () 1) (lambda () 0))";
     assertEquals(-67L, eval(code, env));
   }
+
+  @Test
+  public void testHaversineFormula() {
+    eval("(define earth-radius 6371)", env);
+    eval("(define (distance lat1 long1 lat2 long2)" +
+         "  (define (h a b) (expt (sin (/ (- b a) 2)) 2))" +
+         "  (* 2 earth-radius " +
+         "     (asin (sqrt (+ (h lat1 lat2) " +
+         "                    (* (cos lat1) (cos lat2) (h long1 long2)))))))", env);
+    eval("(define (deg-to-rad d m s) " +
+         "  (* (/ pi 180) (+ d (/ m 60) (/ s 3600))))", env);
+    assertEquals(2886.444442837984,
+                 eval("(distance (deg-to-rad 36  7.2 0) (deg-to-rad  86 40.2 0)" +
+                      "          (deg-to-rad 33 56.4 0) (deg-to-rad 118 24.0 0))", env));
+  }
 }
