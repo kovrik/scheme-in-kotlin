8beeecf50c6bcbc12ed275a172a5bfa772a55c9e
kovrik <kovrik0@gmail.com>
2017-05-22 17:48:08 +1200

    Convert Keyword proc to Kotlin

---
+++ src/main/java/core/procedures/keywords/KeywordProc.kt
@@ -0,0 +1,15 @@
+package core.procedures.keywords
+
+import core.procedures.AFn
+import core.procedures.FnArgsBuilder
+import core.scm.Keyword
+
+class KeywordProc : AFn(FnArgsBuilder().min(1).max(1).mandatory(arrayOf<Class<*>>(CharSequence::class.java)).build()) {
+
+    override val name: String
+        get() = "keyword"
+
+    override fun apply1(arg: Any?): Keyword? {
+        return if (arg == null) null else Keyword.intern(arg.toString())
+    }
+}
