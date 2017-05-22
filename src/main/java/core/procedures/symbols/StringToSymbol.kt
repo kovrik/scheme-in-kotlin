ce04cd727dbb7830b36b2efc86bc9e54ce80d0c2
kovrik <kovrik0@gmail.com>
2017-05-22 16:25:49 +1200

    Convert symbol procs to Kotlin

---
+++ src/main/java/core/procedures/symbols/StringToSymbol.kt
@@ -0,0 +1,18 @@
+package core.procedures.symbols
+
+import core.procedures.AFn
+import core.procedures.FnArgsBuilder
+import core.scm.Symbol
+
+open class StringToSymbol : AFn(FnArgsBuilder().min(1).max(1).mandatory(arrayOf<Class<*>>(CharSequence::class.java)).build()) {
+
+    override val isPure: Boolean
+        get() = true
+
+    override val name: String
+        get() = "string->symbol"
+
+    override fun apply1(arg: Any?): Symbol? {
+        return Symbol.intern(arg!!.toString())
+    }
+}
---
+++ src/main/java/core/procedures/symbols/SymbolToString.kt
@@ -0,0 +1,18 @@
+package core.procedures.symbols
+
+import core.procedures.AFn
+import core.procedures.FnArgsBuilder
+import core.scm.Symbol
+
+class SymbolToString : AFn(FnArgsBuilder().min(1).max(1).mandatory(arrayOf<Class<*>>(Symbol::class.java)).build()) {
+
+    override val isPure: Boolean
+        get() = true
+
+    override val name: String
+        get() = "symbol->string"
+
+    override fun apply1(arg: Any?): String? {
+        return arg!!.toString()
+    }
+}
