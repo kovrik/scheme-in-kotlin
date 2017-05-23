af6c0b89873ac2c285a1f0f76337303a8c3bddb7
kovrik <kovrik0@gmail.com>
2017-05-23 15:34:22 +1200

    Convert AllTests to Kotlin

---
+++ src/test/java/unittests/AllTests.kt
@@ -0,0 +1,33 @@
+package unittests
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import unittests.s7.S7Tests
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(ReaderTest::class,
+        WriterTest::class,
+        EvaluatorTest::class,
+        TCOTest::class,
+        SpecialFormTest::class,
+        ContinuationsTest::class,
+        DelayedTest::class,
+        NumberTest::class,
+        BitwiseTests::class,
+        ComplexTest::class,
+        TrigonometryTest::class,
+        RatioTest::class,
+        CharacterTest::class,
+        StringTest::class,
+        ListTest::class,
+        ConsTest::class,
+        VectorTest::class,
+        IOTest::class,
+        RosettaCodeTest::class,
+        JavaInteropTest::class,
+        HashmapTest::class,
+        KeywordTest::class,
+        ReflectorTest::class,
+        /* S7 Test Suite: https://ccrma.stanford.edu/software/snd/snd/s7.html */
+        S7Tests::class)
+class AllTests
