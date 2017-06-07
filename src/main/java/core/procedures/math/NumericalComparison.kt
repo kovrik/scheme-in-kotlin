3d9d6beb09b53b2023e2c2d494609e40a3bf8c0c
kovrik <kovrik0@gmail.com>
2017-06-08 08:27:22 +1200

    Change NumericalComparison

--- src/main/java/core/procedures/math/NumericalComparison.kt
+++ src/main/java/core/procedures/math/NumericalComparison.kt
@@ -5,30 +5,37 @@ import core.procedures.FnArgs
 import core.scm.Type
 import core.utils.Utils
 
-import java.util.function.BiPredicate
-
-class NumericalComparison private constructor(override val name: String, private val predicate: BiPredicate<Comparable<Number>, Number>) :
+class NumericalComparison private constructor(override val name: String, private val type: ComparisonType) :
         AFn(FnArgs(min = 2, mandatory = arrayOf<Class<*>>(Type.Real::class.java, Type.Real::class.java), rest = Type.Real::class.java)) {
 
+    enum class ComparisonType {
+        EQUAL, LESS, GREATER, LESS_EQUAL, GREATER_EQUAL
+    }
+
     companion object {
-        val EQUAL         = NumericalComparison("=",  BiPredicate { f, s -> f.compareTo(s) == 0 })
-        val LESS          = NumericalComparison("<",  BiPredicate { f, s -> f < s })
-        val GREATER       = NumericalComparison(">",  BiPredicate { f, s -> f > s })
-        val LESS_EQUAL    = NumericalComparison("<=", BiPredicate { f, s -> f <= s })
-        val GREATER_EQUAL = NumericalComparison(">=", BiPredicate { f, s -> f >= s })
+        val EQUAL         = NumericalComparison("=",  ComparisonType.EQUAL)
+        val LESS          = NumericalComparison("<",  ComparisonType.LESS)
+        val GREATER       = NumericalComparison(">",  ComparisonType.GREATER)
+        val LESS_EQUAL    = NumericalComparison("<=", ComparisonType.LESS_EQUAL)
+        val GREATER_EQUAL = NumericalComparison(">=", ComparisonType.GREATER_EQUAL)
     }
 
     override val isPure = true
 
     override operator fun invoke(arg1: Any?, arg2: Any?): Boolean {
         val (f, s) = Utils.upcast(arg1!! as Number, arg2!! as Number)
-        return predicate.test(f as Comparable<Number>, s)
+        return when (type) {
+            ComparisonType.EQUAL         -> f as Comparable<Number> == s
+            ComparisonType.LESS          -> f as Comparable<Number> <  s
+            ComparisonType.GREATER       -> f as Comparable<Number> >  s
+            ComparisonType.LESS_EQUAL    -> f as Comparable<Number> <= s
+            ComparisonType.GREATER_EQUAL -> f as Comparable<Number> >= s
+        }
     }
 
     override operator fun invoke(vararg args: Any?): Boolean {
         for (i in 0..args.size - 2) {
-            val result = invoke(args[i], args[i + 1])
-            if (!result) {
+            if (!invoke(args[i], args[i + 1])) {
                 return false
             }
         }
