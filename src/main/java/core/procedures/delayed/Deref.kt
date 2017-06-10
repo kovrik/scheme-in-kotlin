7d96b72640d2be523995013f72dc31b544e49992
kovrik <kovrik0@gmail.com>
2017-06-10 22:09:01 +1200

    Fix take and deref procedures

--- src/main/java/core/procedures/delayed/Deref.kt
+++ src/main/java/core/procedures/delayed/Deref.kt
@@ -3,7 +3,7 @@ package core.procedures.delayed
 import core.procedures.AFn
 import core.scm.IDeref
 
-class Deref : AFn(name = "deref", minArgs = 1, maxArgs = 1, mandatoryArgsTypes = arrayOf(IDeref::class.java)) {
+class Deref : AFn(name = "deref", minArgs = 1, maxArgs = 1, mandatoryArgsTypes = arrayOf<Class<*>>(IDeref::class.java)) {
 
     override operator fun invoke(arg: Any?) = (arg!! as IDeref).deref()
 }
--- src/main/java/core/procedures/generic/Take.kt
+++ src/main/java/core/procedures/generic/Take.kt
@@ -4,7 +4,7 @@ import core.procedures.AFn
 import core.scm.Cons
 import core.utils.Utils
 
-class Take : AFn(name = "take", isPure = true, minArgs = 2, maxArgs = 2, mandatoryArgsTypes = arrayOf(Int::class.javaObjectType)) {
+class Take : AFn(name = "take", isPure = true, minArgs = 2, maxArgs = 2, mandatoryArgsTypes = arrayOf<Class<*>>(Int::class.javaObjectType)) {
 
     override operator fun invoke(arg1: Any?, arg2: Any?): Any? {
         val count = (arg1 as Number).toInt()
