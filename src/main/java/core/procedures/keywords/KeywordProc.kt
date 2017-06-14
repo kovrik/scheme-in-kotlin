a9606e9dbc4a1434be86850ce82d68b14c4576a8
kovrik <kovrik0@gmail.com>
2017-06-15 11:47:03 +1200

    keyword: add idiomatic null-check

--- src/main/java/core/procedures/keywords/KeywordProc.kt
+++ src/main/java/core/procedures/keywords/KeywordProc.kt
@@ -6,5 +6,5 @@ import core.scm.Keyword
 class KeywordProc : AFn<CharSequence?, Keyword?>(name = "keyword", minArgs = 1, maxArgs = 1,
                                                  mandatoryArgsTypes = arrayOf<Class<*>>(CharSequence::class.java)) {
 
-    override operator fun invoke(arg: CharSequence?) = if (arg == null) null else Keyword.intern(arg.toString())
+    override operator fun invoke(arg: CharSequence?) = arg?.let { Keyword.intern(arg.toString()) }
 }
