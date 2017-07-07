5dd83f8eb1ee2d5128f83f137bfb745682d8fa30
kovrik <kovrik0@gmail.com>
2017-07-07 14:31:03 +1200

    Evaluator: remove unused import

--- src/main/java/core/evaluator/Evaluator.kt
+++ src/main/java/core/evaluator/Evaluator.kt
@@ -4,7 +4,6 @@ import core.environment.Environment
 import core.exceptions.IllegalSyntaxException
 import core.exceptions.ReentrantContinuationException
 import core.procedures.AFn
-import core.procedures.IFn
 import core.procedures.continuations.CalledContinuation
 import core.scm.*
 import core.scm.specialforms.ISpecialForm
