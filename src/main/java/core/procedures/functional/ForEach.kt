39b0c56623924e19ad17939573b062f54d69b506
kovrik <kovrik0@gmail.com>
2017-07-10 09:29:46 +1200

    for-each: simplify

--- src/main/java/core/procedures/functional/ForEach.kt
+++ src/main/java/core/procedures/functional/ForEach.kt
@@ -9,10 +9,7 @@ class ForEach : AFn<Any?, Any>(name = "for-each", minArgs = 2, mandatoryArgsType
 
     object map : MapProc() { override val name = "for-each" }
 
-    override operator fun invoke(args: Array<out Any?>): Thunk {
-        /* For-each is the same as `map`, but ignores the result */
-        val result = map.invoke(args)
-        /* Void (ignore) results: (void <map-results>) */
-        return Thunk(Cons.list(VoidProc.VOID, result))
-    }
+    /* For-each is the same as `map`, but ignores the result:
+     * Void (ignore) results: (void <map-results>) */
+    override operator fun invoke(args: Array<out Any?>) = Thunk(Cons.list(VoidProc.VOID, map(args)))
 }
