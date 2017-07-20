33f607311ca0a91aa75f14e612c9a1e499754b27
kovrik <kovrik0@gmail.com>
2017-07-20 14:13:02 +1200

    Cleanup Type class

--- src/main/java/core/exceptions/WrongTypeException.kt
+++ src/main/java/core/exceptions/WrongTypeException.kt
@@ -1,5 +1,12 @@
 package core.exceptions
 
+import core.procedures.AFn
+import core.procedures.IFn
+import core.scm.BigComplex
+import core.scm.IAssoc
+import core.scm.IDeref
+import core.scm.IPort
+import core.scm.MutableString
 import core.scm.Type
 import core.writer.Writer
 
@@ -8,9 +15,29 @@ class WrongTypeException : IllegalArgumentException {
     constructor(message: String) : super(message)
 
     constructor(name: String, expected: String, given: Any?) : super((if (name.isEmpty()) "#<procedure>" else name) +
-            ": type mismatch; (" + "expected: $expected, given: ${Writer.write(given)})", null)
+                ": type mismatch; (expected: $expected, given: ${Writer.write(given)})", null)
 
-    constructor(name: String, expected: Class<*>, given: Any?) : this(name, Type.nameOf(expected), given)
+    constructor(name: String, expected: Class<*>, given: Any?) : this(name, CUSTOM_TYPE_NAMES[expected] ?: expected.simpleName, given)
 
     @Synchronized override fun fillInStackTrace() = null
+
+    companion object {
+        /* Override type names for some classes */
+        private val CUSTOM_TYPE_NAMES = hashMapOf(
+                Int::class.javaObjectType   to "Integer",
+                Long::class.javaObjectType  to "Integer",
+                Type.Rational::class.java   to "Rational",
+                BigComplex::class.java      to "Complex",
+                CharSequence::class.java    to "String",
+                StringBuilder::class.java   to "MutableString",
+                MutableString::class.java   to "MutableString",
+                IFn::class.java             to "Procedure",
+                AFn::class.java             to "Procedure",
+                Type.ProperList::class.java to "List",
+                IPort::class.java           to "Port",
+                Map.Entry::class.java       to "MapEntry",
+                IAssoc::class.java          to "Associative",
+                IDeref::class.java          to "Delay or Promise or Future"
+        )
+    }
 }
--- src/main/java/core/scm/Type.kt
+++ src/main/java/core/scm/Type.kt
@@ -1,51 +1,18 @@
 package core.scm
 
 import core.exceptions.WrongTypeException
-import core.procedures.AFn
-import core.procedures.IFn
 import core.utils.Utils
 
 object Type {
 
-    /* Marker classes
-     *
-     * The return type of a method declared in an annotation type must be one of the following,
-     * or a compile-time error occurs:
-     * - Primitive type
-     * - String
-     * - Class or an invocation of Class (§4.5)
-     * - Enum type
-     * - Annotation type
-     * - Array type whose component type is one of the preceding types
-     * (see https://docs.oracle.com/javase/specs/jls/se8/html/jls-9.html#jls-9.6.1)
-     */
     // TODO Get rid of them
-    /* Marker classes for Proper and Improper lists */
+    /* Marker objects */
     object ProperList
     object Pair
-    /* Marker classes for numbers */
     object ExactNonNegativeInteger
     object Rational
     object Real
     object BitOp
-
-    /* Override type names for some classes */
-    private val TYPE_NAME_MAPPINGS = hashMapOf(
-            Int::class.javaObjectType  to "Integer",
-            Long::class.javaObjectType to "Integer",
-            Rational::class.java       to "Rational",
-            BigComplex::class.java     to "Complex",
-            CharSequence::class.java   to "String",
-            StringBuilder::class.java  to "MutableString",
-            MutableString::class.java  to "MutableString",
-            IFn::class.java            to "Procedure",
-            AFn::class.java            to "Procedure",
-            ProperList::class.java     to "List",
-            IPort::class.java          to "Port",
-            Map.Entry::class.java      to "MapEntry",
-            IAssoc::class.java         to "Associative",
-            IDeref::class.java         to "Delay or Promise or Future"
-    )
 
     private val TYPE_PREDICATES = hashMapOf(
             String::class.java                  to { o: Any? -> o is CharSequence },
@@ -62,8 +29,6 @@ object Type {
             BitOp::class.java                   to Utils::isBitOpSupported,
             IAssoc::class.java                  to Utils::isAssoc
     )
-
-    fun nameOf(clazz: Class<*>): String = TYPE_NAME_MAPPINGS.getOrDefault(clazz, clazz.simpleName)
 
     fun assertType(name: String, o: Any?, expected: Class<*>) = when {
         o == null -> true
