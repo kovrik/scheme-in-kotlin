f9607d8fed25d5846e41e53a0a2053b0cb41e75b
kovrik <kovrik0@gmail.com>
2017-07-27 16:23:37 +1200

    Change message of ReentrantContinuationException

--- src/main/java/core/exceptions/ReentrantContinuationException.kt
+++ src/main/java/core/exceptions/ReentrantContinuationException.kt
@@ -1,6 +1,6 @@
 package core.exceptions
 
-class ReentrantContinuationException : RuntimeException("Re-entrant continuation: implementation restriction: continuation can only be used once") {
+class ReentrantContinuationException : RuntimeException("implementation restriction - continuation can only be used once") {
 
     @Synchronized override fun fillInStackTrace() = null
 }
