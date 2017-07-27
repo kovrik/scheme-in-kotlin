87259e21f1c4ed34498bca2513a3145be9344755
kovrik <kovrik0@gmail.com>
2017-07-27 14:18:32 +1200

    cast: specify return type explicitly

--- src/main/java/core/procedures/system/Cast.kt
+++ src/main/java/core/procedures/system/Cast.kt
@@ -5,5 +5,5 @@ import core.procedures.AFn
 class Cast : AFn<Any?, Any?>(name = "cast", isPure = true, minArgs = 2, maxArgs = 2,
                  mandatoryArgsTypes = arrayOf(Class::class.java, Any::class.java)) {
 
-    override operator fun invoke(arg1: Any?, arg2: Any?) = (arg1 as Class<*>).cast(arg2)
+    override operator fun invoke(arg1: Any?, arg2: Any?): Any? = (arg1 as Class<*>).cast(arg2)
 }
