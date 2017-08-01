1d5a6fd1bfd82e22789fa8a8d541a9a2d699ebef
kovrik <kovrik0@gmail.com>
2017-08-02 11:42:46 +1200

    Eval: allow nil

--- src/main/java/core/procedures/system/Eval.kt
+++ src/main/java/core/procedures/system/Eval.kt
@@ -5,5 +5,5 @@ import core.scm.Thunk
 
 class Eval : AFn<Any?, Any?>(name = "eval", minArgs = 1, maxArgs = 1) {
 
-    override operator fun invoke(arg: Any?) = Thunk(arg!!)
+    override operator fun invoke(arg: Any?) = Thunk(arg)
 }
