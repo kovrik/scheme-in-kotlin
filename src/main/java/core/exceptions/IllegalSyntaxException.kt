6a6990e602d713c2c4c71f0a11b60686977687c9
kovrik <kovrik0@gmail.com>
2017-08-03 11:43:27 +1200

    IllegalSyntaxException: simplify

--- src/main/java/core/exceptions/IllegalSyntaxException.kt
+++ src/main/java/core/exceptions/IllegalSyntaxException.kt
@@ -5,13 +5,10 @@ class IllegalSyntaxException(message: String) : RuntimeException(message) {
     @Synchronized override fun fillInStackTrace() = null
 
     companion object {
-
-        @JvmOverloads fun of(syntax: String, expression: Any, description: String? = null): IllegalSyntaxException {
-            val message = when (description) {
-                null -> "$syntax: bad syntax in form: $expression"
-                else -> "$syntax: bad syntax ($description) in form: $expression"
-            }
-            return IllegalSyntaxException(message)
-        }
+        @JvmOverloads fun of(syntax: String, expression: Any, description: String? = null) = IllegalSyntaxException(
+                when (description) {
+                    null -> "$syntax: bad syntax in form: $expression"
+                    else -> "$syntax: bad syntax ($description) in form: $expression"
+                })
     }
 }
