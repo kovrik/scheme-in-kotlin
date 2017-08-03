6e4d9dd93c861bd000081e338b67223e2ace8e27
kovrik <kovrik0@gmail.com>
2017-08-04 11:12:32 +1200

    newline: delete LS val

--- src/main/java/core/procedures/io/Newline.kt
+++ src/main/java/core/procedures/io/Newline.kt
@@ -7,12 +7,8 @@ import core.scm.OutputPort
 
 class Newline : AFn<Any?, Unit>(name = "newline", maxArgs = 1, restArgsType = OutputPort::class.java) {
 
-    companion object {
-        private val LS = System.getProperty("line.separator")
-    }
-
     override operator fun invoke(args: Array<out Any?>) {
         val outputPort = if (args.isEmpty()) Repl.currentOutputPort else args[0] as OutputPort
-        outputPort.write(LS)
+        outputPort.writeln("")
     }
 }
