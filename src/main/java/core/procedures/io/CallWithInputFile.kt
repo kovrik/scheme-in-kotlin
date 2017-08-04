88b557dab7dd2784d1a59ad14a5855cbbf4f83f4
kovrik <kovrik0@gmail.com>
2017-08-04 14:34:17 +1200

    call-with-input/output-file: simplify

--- src/main/java/core/procedures/io/CallWithInputFile.kt
+++ src/main/java/core/procedures/io/CallWithInputFile.kt
@@ -2,7 +2,7 @@ package core.procedures.io
 
 import core.procedures.AFn
 import core.procedures.IFn
-import core.scm.Cons
+import core.scm.Cons.Companion.list
 import core.scm.InputPort
 import core.scm.Thunk
 import core.scm.specialforms.Try
@@ -12,11 +12,9 @@ import java.io.FileInputStream
 class CallWithInputFile : AFn<Any?, Any>(name = "call-with-input-file", minArgs = 2, maxArgs = 2,
                                          mandatoryArgsTypes = arrayOf(CharSequence::class.java, IFn::class.java)) {
 
-    override operator fun invoke(arg1: Any?, arg2: Any?): Thunk {
-        /* (try (proc in) (finally (close-input-port in)))*/
-        val inputPort = InputPort(FileInputStream(arg1!!.toString()))
-        val bodyBlock = Cons.list(arg2, inputPort)
-        val finallyBlock = Cons.list(Try.FINALLY, Cons.list(CloseInputPort(), inputPort))
-        return Thunk(Cons.list(Try, bodyBlock, finallyBlock))
+    /* (try (proc in) (finally (close-input-port in)))*/
+    override operator fun invoke(arg1: Any?, arg2: Any?) = InputPort(FileInputStream(arg1!!.toString())).let {
+        Thunk(list(Try, list(arg2, it),
+              list(Try.FINALLY, list(CloseInputPort(), it))))
     }
 }
--- src/main/java/core/procedures/io/CallWithOutputFile.kt
+++ src/main/java/core/procedures/io/CallWithOutputFile.kt
@@ -2,7 +2,7 @@ package core.procedures.io
 
 import core.procedures.AFn
 import core.procedures.IFn
-import core.scm.Cons
+import core.scm.Cons.Companion.list
 import core.scm.OutputPort
 import core.scm.Thunk
 import core.scm.specialforms.Try
@@ -12,11 +12,9 @@ import java.io.FileOutputStream
 class CallWithOutputFile : AFn<Any?, Any>(name = "call-with-output-file", minArgs = 2, maxArgs = 2,
                                           mandatoryArgsTypes = arrayOf(CharSequence::class.java, IFn::class.java)) {
 
-    override operator fun invoke(arg1: Any?, arg2: Any?): Thunk {
-        /* (try (proc out) (finally (close-output-port out)))*/
-        val outputPort = OutputPort(FileOutputStream(arg1!!.toString()))
-        val bodyBlock = Cons.list(arg2, outputPort)
-        val finallyBlock = Cons.list(Try.FINALLY, Cons.list(CloseOutputPort(), outputPort))
-        return Thunk(Cons.list(Try, bodyBlock, finallyBlock))
+    /* (try (proc out) (finally (close-output-port out)))*/
+    override operator fun invoke(arg1: Any?, arg2: Any?) = OutputPort(FileOutputStream(arg1!!.toString())).let {
+        Thunk(list(Try, list(arg2, it),
+              list(Try.FINALLY, list(CloseOutputPort(), it))))
     }
 }
