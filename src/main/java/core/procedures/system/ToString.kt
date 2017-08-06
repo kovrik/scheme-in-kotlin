df441016c0f2bb19f22e66204fce715a65147acc
kovrik <kovrik0@gmail.com>
2017-08-07 08:10:19 +1200

    ->string: lift when argument

--- src/main/java/core/procedures/system/ToString.kt
+++ src/main/java/core/procedures/system/ToString.kt
@@ -5,10 +5,10 @@ import core.writer.Writer
 
 open class ToString : AFn<Any?, CharSequence>(name = "->string", isPure = true) {
 
-    override operator fun invoke(args: Array<out Any?>) = when {
-        args.isEmpty() -> ""
-        args.size == 1 -> str(args[0])
-        else           -> StringBuilder().apply { args.forEach { append(str(it)) } }.toString()
+    override operator fun invoke(args: Array<out Any?>) = when (args.size) {
+        0    -> ""
+        1    -> str(args[0])
+        else -> StringBuilder().apply { args.forEach { append(str(it)) } }.toString()
     }
 
     private fun str(obj: Any?) = when (obj) {
