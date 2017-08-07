80aad0c0271bbaa4c7d591460fa1d73ddc046827
kovrik <kovrik0@gmail.com>
2017-08-07 20:34:45 +1200

    Random: use when

--- src/main/java/core/procedures/system/RandomProc.kt
+++ src/main/java/core/procedures/system/RandomProc.kt
@@ -2,27 +2,28 @@ package core.procedures.system
 
 import core.exceptions.WrongTypeException
 import core.procedures.AFn
+import java.util.*
 
 import java.util.concurrent.ThreadLocalRandom
 
 class RandomProc : AFn<Any?, Number>(name = "random", isPure = true, maxArgs = 2, restArgsType = Long::class.java) {
 
-    override operator fun invoke(args: Array<out Any?>): Number {
-        if (args.isEmpty()) {
-            return Math.random()
-        }
-        if (args.size == 1) {
+    override operator fun invoke(args: Array<out Any?>): Number = when {
+        args.isEmpty() -> Math.random()
+        args.size == 1 -> {
             if (args[0] !is Long || (args[0] as Long) < 1 || args[0] as Long > Int.MAX_VALUE) {
                 throw WrongTypeException(name, "Integer (1 to ${Int.MAX_VALUE})", args[0])
             }
-            return java.util.Random().nextInt((args[0] as Long).toInt()).toLong()
+            Random().nextInt((args[0] as Long).toInt()).toLong()
         }
-        if (args[0] !is Long || (args[0] as Long) < 1 || args[0] as Long > Int.MAX_VALUE) {
-            throw WrongTypeException(name, "Integer (1 to ${Int.MAX_VALUE})", args[0])
+        else -> {
+            if (args[0] !is Long || (args[0] as Long) < 1 || args[0] as Long > Int.MAX_VALUE) {
+                throw WrongTypeException(name, "Integer (1 to ${Int.MAX_VALUE})", args[0])
+            }
+            if (args[1] !is Long || (args[1] as Long) < 1 || args[1] as Long > Int.MAX_VALUE) {
+                throw WrongTypeException(name, "Integer (1 to ${Int.MAX_VALUE})", args[1])
+            }
+            ThreadLocalRandom.current().nextInt((args[0] as Long).toInt(), (args[1] as Long).toInt()).toLong()
         }
-        if (args[1] !is Long || (args[1] as Long) < 1 || args[1] as Long > Int.MAX_VALUE) {
-            throw WrongTypeException(name, "Integer (1 to ${Int.MAX_VALUE})", args[1])
-        }
-        return ThreadLocalRandom.current().nextInt((args[0] as Long).toInt(), (args[1] as Long).toInt()).toLong()
     }
 }
