6452c1c7d21cf3090044ab38d27642ecd204b9a2
kovrik <kovrik0@gmail.com>
2017-08-07 20:38:53 +1200

    Log: move constants to companion object

--- src/main/java/core/procedures/math/Log.kt
+++ src/main/java/core/procedures/math/Log.kt
@@ -10,18 +10,20 @@ import java.math.BigInteger
 class Log : AFn<Number?, Number>(name = "log", isPure = true, minArgs = 1, maxArgs = 1,
                                  mandatoryArgsTypes = arrayOf<Class<*>>(Number::class.java)) {
 
-    /* If number has 307 digits or less, then can use Math.log(double) */
-    private val MAX_DIGITS = 307
-    private val VALUE = Math.log(Math.pow(10.0, MAX_DIGITS.toDouble()))
+    companion object {
+        /* If number has 307 digits or less, then can use Math.log(double) */
+        private val MAX_DIGITS = 307
+        private val VALUE = Math.log(Math.pow(10.0, MAX_DIGITS.toDouble()))
+    }
 
     override operator fun invoke(arg: Number?) = when {
         Utils.isZero(arg) && Utils.isExact(arg) -> throw ArithmeticException("log: undefined for 0")
         Utils.isOne(arg)  && Utils.isExact(arg) -> 0L
-        arg is BigComplex                          -> arg.log()
-        arg is BigRatio                            -> logBig(arg.toBigDecimal())
-        arg is BigDecimal                          -> logBig(arg)
-        arg is BigInteger                          -> logBig(BigDecimal(arg))
-        else                                       -> Math.log(arg!!.toDouble())
+        arg is BigComplex                       -> arg.log()
+        arg is BigRatio                         -> logBig(arg.toBigDecimal())
+        arg is BigDecimal                       -> logBig(arg)
+        arg is BigInteger                       -> logBig(BigDecimal(arg))
+        else                                    -> Math.log(arg!!.toDouble())
     }
 
     /* Natural logarithm for Big numbers (greater than Double.MAX_VALUE) */
