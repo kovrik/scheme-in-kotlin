b061d2a125e43b0dc305cdab78b6cf75bf003668
kovrik <kovrik0@gmail.com>
2017-08-09 07:31:08 +1200

    swap! fixes

--- src/main/java/core/scm/specialforms/Swap.kt
+++ src/main/java/core/scm/specialforms/Swap.kt
@@ -21,16 +21,16 @@ object Swap : SpecialForm("swap!") {
 
         val f = evaluator.eval(form[2], env)
         val fn  = f as? IFn<Any?, Any?> ?: throw WrongTypeException(name, "Procedure", f)
-        val rest = Cons.list<Any?>()
-        if (form.size > 3) {
-            for (i in 3..form.size - 1) {
-                rest.add(evaluator.eval(form[i], env))
-            }
-        }
-        val applyForm  = Cons.list(apply, fn, Cons.list(Quote, rest))
+
         while (true) {
             val oldVal = box.deref()
-            rest.add(0, oldVal)
+            val rest = Cons.list<Any?>(oldVal)
+            if (form.size > 3) {
+                for (i in 3..form.size - 1) {
+                    rest.add(evaluator.eval(form[i], env))
+                }
+            }
+            val applyForm  = Cons.list(apply, fn, Cons.list(Quote, rest))
             val newVal = evaluator.eval(applyForm, env)
             if (box.compareAndSet(oldVal, newVal)) {
                 return newVal
