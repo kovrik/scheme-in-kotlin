999f66b7766329db8aea579afd836b335761e274
kovrik <kovrik0@gmail.com>
2017-08-18 17:27:27 +1200

    sqrt: minor formatting fix

--- src/main/java/core/procedures/math/Sqrt.kt
+++ src/main/java/core/procedures/math/Sqrt.kt
@@ -3,7 +3,8 @@ package core.procedures.math
 import core.procedures.AFn
 import core.scm.BigComplex
 
-class Sqrt : AFn<Number?, Number>(name = "sqrt", isPure = true, minArgs = 1, maxArgs = 1, mandatoryArgsTypes = arrayOf<Class<*>>(Number::class.java)) {
+class Sqrt : AFn<Number?, Number>(name = "sqrt", isPure = true, minArgs = 1, maxArgs = 1,
+                                  mandatoryArgsTypes = arrayOf<Class<*>>(Number::class.java)) {
 
     override operator fun invoke(arg: Number?): Number = when (arg) {
         is BigComplex -> when (arg.im.signum()) {
