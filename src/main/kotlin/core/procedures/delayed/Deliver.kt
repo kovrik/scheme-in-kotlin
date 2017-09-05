423d36454c50526df2ec9f1d21a47196c21e3489
kovrik <kovrik0@gmail.com>
2017-09-05 19:39:27 +1200

    Deliver: don't use this

--- src/main/kotlin/core/procedures/delayed/Deliver.kt
+++ src/main/kotlin/core/procedures/delayed/Deliver.kt
@@ -8,10 +8,10 @@ import java.util.concurrent.CompletableFuture
 class Deliver : AFn<Any?, CompletableFuture<Any>?>(name = "deliver", minArgs = 2, maxArgs = 2,
                                                    mandatoryArgsTypes = arrayOf(Promise::class.java, Any::class.java)) {
 
-    override operator fun invoke(arg1: Any?, arg2: Any?) = (arg1 as Promise?).apply {
+    override operator fun invoke(arg1: Any?, arg2: Any?): Promise? = (arg1!! as Promise).apply {
         when {
-            this!!.isDone || this.isCompletedExceptionally -> return null
-            else -> this.complete(arg2)
+            isDone || isCompletedExceptionally -> return null
+            else -> complete(arg2)
         }
     }
 }
