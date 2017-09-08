c02ccbd2d2a1f271e88afe9f32638ed9a1c7ce5f
kovrik <kovrik0@gmail.com>
2017-09-08 18:08:40 +1200

    time: cpu time, real time, gc time, gc count

--- src/main/java/core/scm/specialforms/Time.kt
+++ src/main/java/core/scm/specialforms/Time.kt
@@ -5,6 +5,7 @@ import core.environment.Environment
 import core.Evaluator
 import core.exceptions.IllegalSyntaxException
 
+import java.lang.management.ManagementFactory
 import java.util.concurrent.TimeUnit
 
 /**
@@ -19,13 +20,32 @@ object Time : SpecialForm("time") {
         if (form.size < 2) {
             throw IllegalSyntaxException(toString(), form)
         }
-        val start = System.nanoTime()
+
+        val threadMXBean = ManagementFactory.getThreadMXBean()
+        val threadId = Thread.currentThread().id
+        val threadCpuTime = threadMXBean.getThreadCpuTime(threadId)
+
+        val gcBeans = ManagementFactory.getGarbageCollectorMXBeans()
+        var gcCount = 0L
+        var gcTimeMillis = 0L
+
+        val nanos = System.nanoTime()
+
         for (i in 1..form.size - 2) {
             evaluator.eval(form[i], env)
         }
         val result = evaluator.eval(form[form.size - 1], env)
-        val diff = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start)
-        Repl.currentOutputPort.writeln("elapsed time: $diff ms")
+
+        for (gcBean in gcBeans) {
+            gcCount += gcBean.collectionCount
+            gcTimeMillis += gcBean.collectionTime
+        }
+
+        val cpuTime = TimeUnit.NANOSECONDS.toMillis(threadMXBean.getThreadCpuTime(threadId) - threadCpuTime)
+        val realTime = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - nanos)
+
+        Repl.currentOutputPort.writeln("CPU Time: $cpuTime ms; Real Time: $realTime ms;" +
+                                       " GC Time: $gcTimeMillis ms; GC Count: $gcCount")
         return result
     }
 }
