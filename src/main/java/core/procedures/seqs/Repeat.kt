28efd227c3075f114ed86f7440e28eb6b61041f4
kovrik <kovrik0@gmail.com>
2017-09-14 07:23:34 +1200

    repeat: swap arg order

--- src/main/java/core/procedures/seqs/Repeat.kt
+++ src/main/java/core/procedures/seqs/Repeat.kt
@@ -1,14 +1,16 @@
 package core.procedures.seqs
 
+import core.exceptions.WrongTypeException
 import core.procedures.AFn
-import core.scm.Type
+import core.utils.Utils
 
-class Repeat : AFn<Any?, Any?>(name = "repeat", isPure = true, minArgs = 1, maxArgs = 2,
-                               mandatoryArgsTypes = arrayOf<Class<*>>(Any::class.java),
-                               restArgsType = Type.Real::class.java) {
+class Repeat : AFn<Any?, Any?>(name = "repeat", minArgs = 1, maxArgs = 2) {
 
     override operator fun invoke(args: Array<out Any?>) = when (args.size) {
         1    -> generateSequence(args[0], { it })
-        else -> generateSequence(args[0], { it }).take((args[1] as Number).toInt())
+        else -> when {
+            !Utils.isReal(args[0]) -> throw WrongTypeException(toString(), "Real", args[0])
+            else -> generateSequence(args[1], { it }).take((args[0] as Number).toInt())
+        }
     }
 }
