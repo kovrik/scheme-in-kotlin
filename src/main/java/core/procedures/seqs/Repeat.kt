5101d7d254119150ee4766def0989cdac915aa87
kovrik <kovrik0@gmail.com>
2017-09-14 07:24:18 +1200

    repeat: fix exception message

--- src/main/java/core/procedures/seqs/Repeat.kt
+++ src/main/java/core/procedures/seqs/Repeat.kt
@@ -9,7 +9,7 @@ class Repeat : AFn<Any?, Any?>(name = "repeat", minArgs = 1, maxArgs = 2) {
     override operator fun invoke(args: Array<out Any?>) = when (args.size) {
         1    -> generateSequence(args[0], { it })
         else -> when {
-            !Utils.isReal(args[0]) -> throw WrongTypeException(toString(), "Real", args[0])
+            !Utils.isReal(args[0]) -> throw WrongTypeException(name, "Real", args[0])
             else -> generateSequence(args[1], { it }).take((args[0] as Number).toInt())
         }
     }
