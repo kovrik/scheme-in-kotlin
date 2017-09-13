00d3f1498542a5fa34ec9fcd2ac8a80e70e9a961
kovrik <kovrik0@gmail.com>
2017-09-13 21:45:02 +1200

    repeat: support second arg

--- src/main/java/core/procedures/seqs/Repeat.kt
+++ src/main/java/core/procedures/seqs/Repeat.kt
@@ -1,8 +1,14 @@
 package core.procedures.seqs
 
 import core.procedures.AFn
+import core.scm.Type
 
-class Repeat : AFn<Any?, Any?>(name = "repeat", isPure = true) {
+class Repeat : AFn<Any?, Any?>(name = "repeat", isPure = true, minArgs = 1, maxArgs = 2,
+                               mandatoryArgsTypes = arrayOf<Class<*>>(Any::class.java),
+                               restArgsType = Type.Real::class.java) {
 
-    override operator fun invoke(args: Array<out Any?>) = generateSequence(args[0], { it })
+    override operator fun invoke(args: Array<out Any?>) = when (args.size) {
+        1    -> generateSequence(args[0], { it })
+        else -> generateSequence(args[0], { it }).take((args[1] as Number).toInt())
+    }
 }
