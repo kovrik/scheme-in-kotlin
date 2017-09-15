f6ca33f17ba3f6c199b54ead11af2efcadf004bc
kovrik <kovrik0@gmail.com>
2017-09-15 17:13:39 +1200

    Simplify round

--- src/main/java/core/procedures/math/Round.kt
+++ src/main/java/core/procedures/math/Round.kt
@@ -3,19 +3,20 @@ package core.procedures.math
 import core.procedures.AFn
 import core.scm.BigRatio
 import core.scm.Type
-import core.utils.Utils
 
 import java.math.BigDecimal
 import java.math.BigInteger
-import java.math.MathContext
+import java.math.RoundingMode
 
-class Round : AFn<Number?, Number>(name = "round", isPure = true, minArgs = 1, maxArgs = 1, mandatoryArgsTypes = arrayOf<Class<*>>(Type.Real::class.java)) {
+class Round : AFn<Number?, Number>(name = "round", isPure = true, minArgs = 1, maxArgs = 1,
+                                   mandatoryArgsTypes = arrayOf<Class<*>>(Type.Real::class.java)) {
 
     override operator fun invoke(arg: Number?) = round(arg!!)!!
 
+    // TODO Round to EVEN ?
     private fun round(number: Number) = when (number) {
         is Long, is Int, is Short, is Byte, is BigInteger -> number
-        is BigDecimal -> if (number.scale() == 0) number.round(MathContext.UNLIMITED) else number.round(Utils.DEFAULT_CONTEXT)
+        is BigDecimal -> number.setScale(0, RoundingMode.HALF_EVEN).setScale(1)
         is BigRatio   -> number.round()
         else          -> Math.rint(number.toDouble())
     }
