eb7ac654172b1af2ca0fb845bbf343b1413c3c7a
kovrik <kovrik0@gmail.com>
2017-09-17 22:35:32 +1200

    cycle: check if seq has no elements

--- src/main/java/core/procedures/seqs/Cycle.kt
+++ src/main/java/core/procedures/seqs/Cycle.kt
@@ -6,9 +6,9 @@ import core.utils.Utils
 
 class Cycle : AFn<Any?, Any?>(name = "cycle", minArgs = 1, maxArgs = 1) {
 
-    // TODO make it return emptySequence if seq is empty!
     override operator fun invoke(arg: Any?): Sequence<Any?> {
         val seq = Utils.toSequence(arg)
-        return LazySeq(seq.let { generateSequence(it) { it }.flatten() })
+        if (!seq.iterator().hasNext()) return emptySequence()
+        return LazySeq(generateSequence(seq) { seq }.flatten())
     }
 }
