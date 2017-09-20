31b34d700bdac5986341c8028d7843dc5edaf27e
kovrik <kovrik0@gmail.com>
2017-09-20 22:06:05 +1200

    force: return arg if it is not a delay

--- src/main/java/core/procedures/delayed/Force.kt
+++ src/main/java/core/procedures/delayed/Force.kt
@@ -1,18 +1,12 @@
 package core.procedures.delayed
 
-import core.exceptions.WrongTypeException
 import core.procedures.AFn
-import core.scm.Box
 import core.scm.Delay
-import core.scm.Future
-import core.scm.Promise
 
 class Force : AFn<Any?, Any?>(name = "force", minArgs = 1, maxArgs = 1) {
 
     override operator fun invoke(arg: Any?) = when (arg) {
-        is Future, is Promise, is Box<*>, is Thread -> arg
-        /* Force derefs delays */
         is Delay -> arg.deref()
-        else -> throw WrongTypeException(name, "Delay or Promise or Future or Box", arg)
+        else     -> arg
     }
 }
