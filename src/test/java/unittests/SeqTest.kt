9192282920c7cf588435886b3c7f754bf3f159dd
kovrik <kovrik0@gmail.com>
2017-09-20 17:15:05 +1200

    Additional Range tests

--- src/test/java/unittests/SeqTest.kt
+++ src/test/java/unittests/SeqTest.kt
@@ -140,5 +140,13 @@ class SeqTest : AbstractTest() {
         assertEquals(listOf(0.0, 0.3, 0.6, 0.8999999999999999), eval("(into '() (range 0 1 0.3))", env))
         assertEquals(listOf(0L, BigRatio.valueOf("1", "2"), BigRatio.ONE, BigRatio.valueOf("3", "2")),
                      eval("(into '() (range 0 2 1/2))", env))
+        assertEquals(listOf<Nothing>(),     eval("(into '() (range 0))", env))
+        assertEquals(listOf<Nothing>(),     eval("(into '() (range 0.0))", env))
+        assertEquals(listOf<Nothing>(),     eval("(into '() (range (bigint 0)))", env))
+        assertEquals(listOf<Nothing>(),     eval("(into '() (range 0/1))", env))
+        assertEquals(listOf<Nothing>(),     eval("(into '() (range 5 5))", env))
+        assertEquals(listOf<Nothing>(),     eval("(into '() (range 5.0 5.0))", env))
+        assertEquals(listOf<Nothing>(),     eval("(into '() (range 5 5 1/2))", env))
+        assertEquals(listOf<Nothing>(),     eval("(into '() (range (bigint 5) (bigint 5)))", env))
     }
 }
