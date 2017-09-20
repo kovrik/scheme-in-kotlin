f6fbc32656f8f447549e855f1d515fdafed28772
kovrik <kovrik0@gmail.com>
2017-09-20 17:28:56 +1200

    for-each: use seqs

--- src/main/java/core/procedures/functional/ForEach.kt
+++ src/main/java/core/procedures/functional/ForEach.kt
@@ -3,12 +3,12 @@ package core.procedures.functional
 import core.procedures.AFn
 import core.procedures.IFn
 
-class ForEach : AFn<Any?, Any>(name = "for-each", minArgs = 2, mandatoryArgsTypes = arrayOf<Class<*>>(IFn::class.java)) {
+class ForEach : AFn<Any?, Any?>(name = "for-each", minArgs = 2, mandatoryArgsTypes = arrayOf<Class<*>>(IFn::class.java)) {
 
     private object map : MapProc() { override val name = "for-each" }
     private val void = VoidProc()
 
     /* For-each is the same as `map`, but ignores the result:
      * Void (ignore) results: (void <map-results>) */
-    override operator fun invoke(args: Array<out Any?>) = void(arrayOf(map(args)))
+    override operator fun invoke(args: Array<out Any?>) = map(args).forEach(void)
 }
--- src/main/java/core/procedures/functional/VoidProc.kt
+++ src/main/java/core/procedures/functional/VoidProc.kt
@@ -5,4 +5,11 @@ import core.procedures.AFn
 class VoidProc : AFn<Any?, Unit>(name = "void", isPure = true) {
 
     override operator fun invoke(args: Array<out Any?>) = Unit
+    override operator fun invoke() = Unit
+    override operator fun invoke(arg: Any?) = Unit
+    override operator fun invoke(arg1: Any?, arg2: Any?) = Unit
+    override operator fun invoke(arg1: Any?, arg2: Any?, arg3: Any?) = Unit
+    override operator fun invoke(arg1: Any?, arg2: Any?, arg3: Any?, arg4: Any?) = Unit
+    override fun checkArity(args: Array<out Any?>) = Unit
+    override fun checkArgs(args: Array<out Any?>) = Unit
 }
