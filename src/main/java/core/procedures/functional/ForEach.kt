d70b46101e01147073fc67ce82e4b9bb9ffa7a30
kovrik <kovrik0@gmail.com>
2017-09-20 17:34:12 +1200

    Fix for-each

--- src/main/java/core/procedures/functional/ForEach.kt
+++ src/main/java/core/procedures/functional/ForEach.kt
@@ -2,13 +2,18 @@ package core.procedures.functional
 
 import core.procedures.AFn
 import core.procedures.IFn
+import core.scm.Cons
+import core.scm.Thunk
 
+/* For-each is the same as `map`, but ignores the result: Void (ignore) results: (void <map-results>) */
 class ForEach : AFn<Any?, Any?>(name = "for-each", minArgs = 2, mandatoryArgsTypes = arrayOf<Class<*>>(IFn::class.java)) {
 
     private object map : MapProc() { override val name = "for-each" }
     private val void = VoidProc()
+    private val apply = Apply()
 
-    /* For-each is the same as `map`, but ignores the result:
-     * Void (ignore) results: (void <map-results>) */
-    override operator fun invoke(args: Array<out Any?>) = map(args).forEach(void)
+//    override operator fun invoke(args: Array<out Any?>) = map(args).forEach(void)
+
+    // TODO Simplify
+    override operator fun invoke(args: Array<out Any?>) = Thunk(Cons.list(apply, void, map(args)))
 }
