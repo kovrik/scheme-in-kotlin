6145f7a0f70a5f6ddc2e996d5bee1a4e4e91909f
kovrik <kovrik0@gmail.com>
2017-09-21 17:44:56 +1200

    inexact->exact: overflow fix

--- src/main/java/core/procedures/math/ToExact.kt
+++ src/main/java/core/procedures/math/ToExact.kt
@@ -9,7 +9,7 @@ import java.math.BigDecimal
 import java.math.BigInteger
 
 class ToExact : AFn<Number?, Number>(name = "inexact->exact", isPure = true, minArgs = 1, maxArgs = 1,
-                    mandatoryArgsTypes = arrayOf<Class<*>>(Number::class.java)) {
+                                     mandatoryArgsTypes = arrayOf<Class<*>>(Number::class.java)) {
 
     override operator fun invoke(arg: Number?): Number = when {
         Utils.isZero(arg) -> arg as Number
@@ -23,27 +23,32 @@ class ToExact : AFn<Number?, Number>(name = "inexact->exact", isPure = true, min
     private fun doubleToExact(number: Double): Number {
         if (!number.isFinite()) throw ArithmeticException("inexact->exact: no exact representation of: ${Writer.write(number)}")
         /* Check if Double is integral */
-        val bits = java.lang.Double.doubleToLongBits(number)
-        val sign = bits.ushr(63)
-        val exponent = (bits.ushr(52) xor (sign shl 11)) - 1023
-        val fraction = bits shl 12
-        var a = 1L
-        var b = 1L
-        for (i in 63 downTo 12) {
-            a = a * 2 + (fraction.ushr(i) and 1)
-            b *= 2
+        try {
+            val bits = java.lang.Double.doubleToLongBits(number)
+            val sign = bits.ushr(63)
+            val exponent = (bits.ushr(52) xor (sign shl 11)) - 1023
+            val fraction = bits shl 12
+            var a = 1L
+            var b = 1L
+            for (i in 63 downTo 12) {
+                a = Math.addExact(Math.multiplyExact(a, 2), (fraction.ushr(i) and 1))
+                b = Math.multiplyExact(b, 2)
+            }
+            if (exponent > 0) {
+                a = Math.multiplyExact(a, (1 shl exponent.toInt()).toLong())
+            } else {
+                b = Math.multiplyExact(b, (1 shl -exponent.toInt()).toLong())
+            }
+            if (sign == 1L) {
+                a = Math.multiplyExact(a, -1)
+            }
+            return BigRatio.valueOf(BigInteger.valueOf(a), BigInteger.valueOf(b))
+        } catch (e: ArithmeticException) {
+            return bigDecimalToExact(BigDecimal(number.toString()))
         }
-        if (exponent > 0) {
-            a *= (1 shl exponent.toInt()).toLong()
-        } else {
-            b *= (1 shl -exponent.toInt()).toLong()
-        }
-        if (sign == 1L) {
-            a *= -1
-        }
-        return BigRatio.valueOf(BigInteger.valueOf(a), BigInteger.valueOf(b))
     }
 
+    // FIXME Use the same algorithm as for Doubles!
     private fun bigDecimalToExact(number: BigDecimal) = when {
         number.scale() > 0 -> BigRatio.valueOf(number.unscaledValue(), BigInteger.TEN.pow(number.scale()))
         else -> BigRatio.valueOf(number.unscaledValue().multiply(BigInteger.TEN.pow(-number.scale())), BigInteger.ONE)
