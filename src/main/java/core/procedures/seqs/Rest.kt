c4a1b5e9de30bc64a8088656e593657ffa317be0
kovrik <kovrik0@gmail.com>
2017-09-25 16:45:29 +1300

    rest: set return type to Sequence

--- src/main/java/core/procedures/seqs/Rest.kt
+++ src/main/java/core/procedures/seqs/Rest.kt
@@ -5,5 +5,5 @@ import core.utils.Utils
 
 class Rest : AFn<Any?, Any?>(name = "rest", isPure = true, minArgs = 1, maxArgs = 1) {
 
-    override operator fun invoke(arg: Any?): Any? = Utils.toSequence(arg).drop(1)
+    override operator fun invoke(arg: Any?) = Utils.toSequence(arg).drop(1)
 }
