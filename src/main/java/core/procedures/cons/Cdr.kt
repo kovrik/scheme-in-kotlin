5985c4fbe2de0e3081658ac3a5efb04d684b35ed
kovrik <kovrik0@gmail.com>
2017-09-25 16:44:56 +1300

    cdr: use isProperList predicate

--- src/main/java/core/procedures/cons/Cdr.kt
+++ src/main/java/core/procedures/cons/Cdr.kt
@@ -1,6 +1,7 @@
 package core.procedures.cons
 
 import core.procedures.AFn
+import core.procedures.predicates.Predicate
 import core.scm.Cons
 import core.scm.MutablePair
 import core.scm.Type
@@ -9,7 +10,7 @@ class Cdr : AFn<Any?, Any?>(name = "cdr", isPure = true, minArgs = 1, maxArgs = 
                             mandatoryArgsTypes = arrayOf<Class<*>>(Type.Pair::class.java)) {
 
     override operator fun invoke(arg: Any?) = when (arg) {
-        is Cons<*>     -> if (arg.isProperList) arg.subList(1, arg.size) else (arg.last() as Any)
+        is Cons<*>     -> if (Predicate.isProperList(arg)) arg.subList(1, arg.size) else (arg.last() as Any)
         is MutablePair -> arg.second
         else           -> (arg as List<*>).subList(1, arg.size)
     }
