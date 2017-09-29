5f838a21d683891b730ea227e834fab7ce4e8e32
kovrik <kovrik0@gmail.com>
2017-09-29 16:45:30 +1300

    Simplify LazySeq

--- src/main/java/core/scm/LazySeq.kt
+++ src/main/java/core/scm/LazySeq.kt
@@ -4,27 +4,22 @@ import core.Evaluator
 import core.environment.Environment
 import core.utils.Utils
 import core.utils.cached
-import java.util.concurrent.atomic.AtomicBoolean
 
 class LazySeq(private val form: List<Any?>, private val env: Environment, private val evaluator: Evaluator) : Sequence<Any?> {
 
-    private val realized = AtomicBoolean(false)
+    private var realized = false
 
-    fun isRealized() = realized.get()
+    fun isRealized() = realized
 
-    // TODO Check if .cached() is OK here
     override fun iterator() = seq.cached().iterator()
 
-    // TODO is it thread-safe? What if it throws an exception?
+    // TODO What if it throws an exception?
     private val seq: Sequence<Any?> by lazy {
-        if (realized.compareAndSet(false, true)) {
-            for (i in 1..form.size - 2) { evaluator.eval(form[i], env) }
-        }
+        for (i in 1..form.size - 2) { evaluator.eval(form[i], env) }
+        realized = true
         when (form.size) {
-            1    -> emptySequence<Nothing>()
+            1 -> emptySequence<Nothing>()
             else -> Utils.toSequence(evaluator.eval(form.last(), env))
         }
     }
 }
-
-
