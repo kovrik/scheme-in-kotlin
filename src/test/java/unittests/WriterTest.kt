ed64e516cddc63fc244157f86e5a5fbab0069261
kovrik <kovrik0@gmail.com>
2018-05-01 17:07:36 +1200

    Additional Writer Unit tests

--- src/test/java/unittests/WriterTest.kt
+++ src/test/java/unittests/WriterTest.kt
@@ -1,8 +1,14 @@
 package unittests
 
+import core.environment.Environment
+import core.scm.*
+import core.scm.specialforms.If
 import core.writer.Writer
 import org.junit.Test
 import org.junit.Assert.assertEquals
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.util.regex.Pattern
 
 class WriterTest {
 
@@ -11,6 +17,7 @@ class WriterTest {
         assertEquals("\"test string\"", Writer.write("test string"))
         assertEquals("\"\"", Writer.write(""))
         assertEquals("\"(1 2 3)\"", Writer.write("(1 2 3)"))
+        assertEquals("\"12ሴ\\\"\\n\"", Writer.write("12\u1234\"\n"))
     }
 
     @Test
@@ -18,6 +25,7 @@ class WriterTest {
         assertEquals("#\\a", Writer.write('a'))
         assertEquals("#\\b", Writer.write('b'))
         assertEquals("#\\Z", Writer.write('Z'))
+        assertEquals("#\\ሴ", Writer.write('\u1234'))
     }
 
     @Test
@@ -85,5 +93,96 @@ class WriterTest {
         assertEquals("[nil, nil]", Writer.write(arrayOf<Any?>(null, null)))
     }
 
-    // TODO Vector, Sequence, Pair, Regex, Thread, Map.Entry, Pattern, CharSequence, Number, Symbol, Keyword, Class, Boolean, Special Cases etc.
+    @Test
+    fun testWriteVector() {
+        assertEquals("[]", Writer.write(Vector.EMPTY))
+        assertEquals("[]", Writer.write(MutableVector.EMPTY))
+        assertEquals("[#t #f #t]", Writer.write(Vector(arrayOf(true, false, true))))
+        assertEquals("[#t #f #t]", Writer.write(MutableVector(arrayOf(true, false, true))))
+    }
+
+    @Test
+    fun testWriteSequence() {
+        assertEquals("()", Writer.write(emptySequence<Nothing>()))
+        assertEquals("(#t #f #t)", Writer.write(listOf(true, false, true).asSequence()))
+    }
+
+    @Test
+    fun testWriteThread() {
+        assertEquals("#<thread:test>", Writer.write(Thread("test")))
+    }
+
+    @Test
+    fun testWriteClass() {
+        assertEquals("#<class:java.lang.String>", Writer.write(String::class.java))
+        assertEquals("#<class:int[]>", Writer.write(IntArray::class.java))
+    }
+
+    @Test
+    fun testWriteSymbol() {
+        assertEquals("test", Writer.write(Symbol.intern("test")))
+        assertEquals("|'|", Writer.write(Symbol.intern("'")))
+        assertEquals("if", Writer.write(If.symbol))
+        assertEquals("#<procedure>", Writer.write(Procedure("", emptyArray(), Any(), Environment(0, null), true)))
+        assertEquals("#<procedure:test>", Writer.write(Procedure("test", emptyArray(), Any(), Environment(0, null), true)))
+    }
+
+    @Test
+    fun testWriteKeyword() {
+        assertEquals(":test", Writer.write(Keyword.intern("test")))
+        assertEquals(":'", Writer.write(Keyword.intern("'")))
+    }
+
+    @Test
+    fun testWriteBoolean() {
+        assertEquals("#t", Writer.write(true))
+        assertEquals("#f", Writer.write(false))
+    }
+
+    @Test
+    fun testWritePattern() {
+        assertEquals("#\"abc\"", Writer.write(Pattern.compile("abc")))
+    }
+
+    @Test
+    fun testWriteRegex() {
+        assertEquals("#\"abc\"", Writer.write(Regex("abc")))
+    }
+
+    @Test
+    fun testWriteMapEntry() {
+        assertEquals("[\"test\" #t]", Writer.write(mapOf(Pair("test", true)).entries.first()))
+    }
+
+    @Test
+    fun testWritePair() {
+        assertEquals("(pair 1 #t)", Writer.write(Pair(1, true)))
+        assertEquals("(mcons 1 #t)", Writer.write(MutablePair(1, true)))
+    }
+
+    @Test
+    fun testWriteNumber() {
+        assertEquals("+nan.0", Writer.write(Double.NaN))
+        assertEquals("+nan.0", Writer.write(Float.NaN))
+        assertEquals("+inf.0", Writer.write(Double.POSITIVE_INFINITY))
+        assertEquals("+inf.0", Writer.write(Float.POSITIVE_INFINITY))
+        assertEquals("-inf.0", Writer.write(Double.NEGATIVE_INFINITY))
+        assertEquals("-inf.0", Writer.write(Float.NEGATIVE_INFINITY))
+        assertEquals("1", Writer.write(1))
+        assertEquals("-1", Writer.write(-1))
+        assertEquals("1", Writer.write(1L))
+        assertEquals("-1", Writer.write(-1L))
+        assertEquals("1.0", Writer.write(1.0))
+        assertEquals("1.0", Writer.write(1.0f))
+        assertEquals("-1.0", Writer.write(-1.0))
+        assertEquals("-1.0", Writer.write(-1.0f))
+        assertEquals("10", Writer.write(BigDecimal.TEN))
+        assertEquals("10", Writer.write(BigInteger.TEN))
+        assertEquals("0", Writer.write(BigRatio.ZERO))
+        assertEquals("1", Writer.write(BigRatio.ONE))
+        assertEquals("1/2", Writer.write(BigRatio.valueOf("4", "8")))
+        assertEquals("-1/2", Writer.write(BigRatio.valueOf("-4", "8")))
+        assertEquals("0+1i", Writer.write(BigComplex.I))
+        assertEquals("-1+0i", Writer.write(BigComplex.I.times(BigComplex.I)))
+    }
 }
