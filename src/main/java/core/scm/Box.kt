a55dff695ff773a430189fb9096b8a34e0b4215a
kovrik <kovrik0@gmail.com>
2018-05-05 09:24:05 +1200

    Remove some explicit return types

--- src/main/java/core/scm/Box.kt
+++ src/main/java/core/scm/Box.kt
@@ -7,7 +7,7 @@ class Box<T>(value: T) : AtomicReference<T>(value), IDeref {
 
     override fun deref(): T = get()
 
-    override fun deref(timeout: Long, timeoutVal: Any?): Any? = throw UnsupportedOperationException("deref with timeout is not available for boxes!")
+    override fun deref(timeout: Long, timeoutVal: Any?) = throw UnsupportedOperationException("deref with timeout is not available for boxes!")
 
     override fun toString() = "#<box!" + Writer.write(get()) + '>'
 }
--- src/main/java/core/scm/WeakBox.kt
+++ src/main/java/core/scm/WeakBox.kt
@@ -7,7 +7,7 @@ class WeakBox<T>(value: T) : WeakReference<T>(value), IDeref {
 
     override fun deref(): T? = get()
 
-    override fun deref(timeout: Long, timeoutVal: Any?): Any? = throw UnsupportedOperationException("deref with timeout is not available for boxes!")
+    override fun deref(timeout: Long, timeoutVal: Any?) = throw UnsupportedOperationException("deref with timeout is not available for boxes!")
 
     override fun toString() = "#<weak-box!" + Writer.write(get()) + '>'
 }
