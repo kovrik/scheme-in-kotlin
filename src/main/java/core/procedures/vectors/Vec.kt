d39baa0778a99b4f67dba2b5b55960d322857c64
kovrik <kovrik0@gmail.com>
2018-05-08 16:52:53 +1200

    Simplify Vec

--- src/main/java/core/procedures/vectors/Vec.kt
+++ src/main/java/core/procedures/vectors/Vec.kt
@@ -6,16 +6,13 @@ import core.scm.MutableVector
 
 class Vec : AFn<Any?, MutableVector>(name = "vec", isPure = true, minArgs = 1, maxArgs = 1) {
 
-    override operator fun invoke(arg: Any?): MutableVector {
-        when (arg) {
-            is Collection<*> -> return MutableVector(arg.toTypedArray())
-            is CharSequence -> {
-                val size = arg.length
-                val vector = MutableVector(size, null)
-                for (i in 0 until size) { vector[i] = arg[i] }
-                return vector
+    override operator fun invoke(arg: Any?) = when (arg) {
+        is Collection<*> -> MutableVector(arg.toTypedArray())
+        is CharSequence -> MutableVector(arg.length, null).apply {
+            for (i in 0 until arg.length) {
+                this[i] = arg[i]
             }
-            else -> throw WrongTypeException(name, "List or Vector or String", arg)
         }
+        else -> throw WrongTypeException(name, "List or Vector or String", arg)
     }
 }
