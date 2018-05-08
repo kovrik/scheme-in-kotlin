35df7976d13a23c0863074dd4d3a79f579ce19bf
kovrik <kovrik0@gmail.com>
2018-05-08 16:45:29 +1200

    Simplify Cycle proc

--- src/main/java/core/procedures/seqs/CycleProc.kt
+++ src/main/java/core/procedures/seqs/CycleProc.kt
@@ -2,14 +2,11 @@ package core.procedures.seqs
 
 import core.procedures.AFn
 import core.scm.Cycle
-import core.scm.ThunkSeq
 import core.utils.Utils
 
 class CycleProc : AFn<Any?, Any?>(name = "cycle", minArgs = 1, maxArgs = 1) {
 
-    override operator fun invoke(arg: Any?): Sequence<Any?> {
-        val seq = Utils.toSequence(arg)
-        if (!seq.iterator().hasNext()) return emptySequence()
-        return ThunkSeq(Cycle(seq))
+    override operator fun invoke(arg: Any?): Sequence<Any?> = Utils.toSequence(arg).let {
+        return if (it.iterator().hasNext()) Cycle(it) else emptySequence()
     }
 }
