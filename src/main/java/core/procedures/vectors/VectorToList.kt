d7b8b490add6ddb686b16e623de939822161f826
kovrik <kovrik0@gmail.com>
2018-05-08 16:46:59 +1200

    Simplify vector->list

--- src/main/java/core/procedures/vectors/VectorToList.kt
+++ src/main/java/core/procedures/vectors/VectorToList.kt
@@ -6,5 +6,5 @@ import core.scm.Vector
 class VectorToList : AFn<Vector, List<*>>(name = "vector->list", isPure = true, minArgs = 1, maxArgs = 1,
                                           mandatoryArgsTypes = arrayOf(Vector::class.java)) {
 
-    override operator fun invoke(arg: Vector) = arg.getArray().toList()
+    override operator fun invoke(arg: Vector) = arg.toList()
 }
