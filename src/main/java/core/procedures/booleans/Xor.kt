2182a9229ed503712fcad47c2ac6ffa07c6cd44f
kovrik <kovrik0@gmail.com>
2018-05-08 16:44:50 +1200

    Xor: use when

--- src/main/java/core/procedures/booleans/Xor.kt
+++ src/main/java/core/procedures/booleans/Xor.kt
@@ -4,9 +4,9 @@ import core.procedures.AFn
 
 class Xor : AFn<Any?, Any?>(name = "xor", isPure = true, minArgs = 2, maxArgs = 2) {
 
-    override operator fun invoke(arg1: Any?, arg2: Any?): Any? {
-        if (arg1 == false && arg2 != false) return arg2
-        if (arg2 == false && arg1 != false) return arg1
-        return false
+    override operator fun invoke(arg1: Any?, arg2: Any?) = when {
+        arg1 == false && arg2 != false -> arg2
+        arg2 == false && arg1 != false -> arg1
+        else -> false
     }
 }
