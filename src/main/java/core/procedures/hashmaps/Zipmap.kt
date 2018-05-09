9faa098ad0b954d4b02b8f7b273b83d63a71a3a2
kovrik <kovrik0@gmail.com>
2018-05-09 16:58:57 +1200

    zipmap: simplify

--- src/main/java/core/procedures/hashmaps/Zipmap.kt
+++ src/main/java/core/procedures/hashmaps/Zipmap.kt
@@ -1,11 +1,10 @@
 package core.procedures.hashmaps
 
 import core.procedures.AFn
-import core.scm.Hashmap
+import core.scm.MutableHashmap
 import core.utils.Utils
 
 class Zipmap : AFn<Any?, Map<*, *>>(name = "zipmap", isPure = true, minArgs = 2, maxArgs = 2) {
 
-    override operator fun invoke(arg1: Any?, arg2: Any?) = Hashmap(Utils.toSequence(arg1).zip(
-                                                                   Utils.toSequence(arg2)).toMap().toMutableMap())
+    override operator fun invoke(arg1: Any?, arg2: Any?) = Utils.toSequence(arg1).zip(Utils.toSequence(arg2)).toMap(MutableHashmap())
 }
