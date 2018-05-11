393fd198e8fc7364ca8f15738ce01453841f0d2d
kovrik <kovrik0@gmail.com>
2018-05-11 23:20:57 +1200

    Fix Box.toString()

--- src/main/java/core/scm/Box.kt
+++ src/main/java/core/scm/Box.kt
@@ -9,5 +9,10 @@ class Box<T>(value: T) : AtomicReference<T>(value), IDeref {
 
     override fun deref(timeout: Long, timeoutVal: Any?) = throw UnsupportedOperationException("deref with timeout is not available for boxes!")
 
-    override fun toString() = "#<box!" + Writer.write(get()) + '>'
+    override fun toString() = get().let {
+        when (it) {
+            this -> "#<box!(this box)>"
+            else -> "#<box!${Writer.write(it)}>"
+        }
+    }
 }
