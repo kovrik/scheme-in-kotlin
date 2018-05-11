43f9926876ccd2f932c1294ba06a152c45852bb8
kovrik <kovrik0@gmail.com>
2018-05-11 23:11:44 +1200

    Conj fixes for arrays

--- src/main/java/core/procedures/seqs/Conj.kt
+++ src/main/java/core/procedures/seqs/Conj.kt
@@ -14,41 +14,65 @@ class Conj : AFn<Any?, Any?>(name = "conj", minArgs = 1) {
             is List<*>     -> first.plus(args.copyOfRange(1, args.size)).toList()
             is Set<*>      -> MutableSet(first.plus(args.copyOfRange(1, args.size)))
             is Vector      -> MutableVector(first.plus(args.copyOfRange(1, args.size)))
-            is ByteArray   -> ByteArray(first.size + args.size - 1).apply {
-                for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
-            }
             is BooleanArray -> BooleanArray(first.size + args.size - 1).apply {
                 for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
+                for (i in 1 until args.size) {
+                    val e = args[i] as? Boolean ?: throw WrongTypeException(name, Boolean::class.javaObjectType, args[i])
+                    this[first.size + i - 1] = e
+                }
+            }
+            is ByteArray -> ByteArray(first.size + args.size - 1).apply {
+                for (i in 0 until first.size) { this[i] = first[i] }
+                for (i in 1 until args.size) {
+                    val e = (args[i] as? Number)?.toByte() ?: throw WrongTypeException(name, Int::class.javaObjectType, args[i])
+                    this[first.size + i - 1] = e
+                }
             }
             is CharArray -> CharArray(first.size + args.size - 1).apply {
                 for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
+                for (i in 1 until args.size) {
+                    val e = (args[i] as? Number)?.toChar() ?: throw WrongTypeException(name, Char::class.javaObjectType, args[i])
+                    this[first.size + i - 1] = e
+                }
             }
             is ShortArray -> ShortArray(first.size + args.size - 1).apply {
                 for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
+                for (i in 1 until args.size) {
+                    val e = (args[i] as? Number)?.toShort() ?: throw WrongTypeException(name, Int::class.javaObjectType, args[i])
+                    this[first.size + i - 1] = e
+                }
             }
             is IntArray -> IntArray(first.size + args.size - 1).apply {
                 for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
+                for (i in 1 until args.size) {
+                    val e = (args[i] as? Number)?.toInt() ?: throw WrongTypeException(name, Int::class.javaObjectType, args[i])
+                    this[first.size + i - 1] = e
+                }
             }
             is LongArray -> LongArray(first.size + args.size - 1).apply {
                 for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
+                for (i in 1 until args.size) {
+                    val e = (args[i] as? Number)?.toLong() ?: throw WrongTypeException(name, Long::class.javaObjectType, args[i])
+                    this[first.size + i - 1] = e
+                }
             }
             is DoubleArray -> DoubleArray(first.size + args.size - 1).apply {
                 for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
+                for (i in 1 until args.size) {
+                    val e = (args[i] as? Number)?.toDouble() ?: throw WrongTypeException(name, Type.Real::class.java, args[i])
+                    this[first.size + i - 1] = e
+                }
             }
             is FloatArray -> FloatArray(first.size + args.size - 1).apply {
                 for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
+                for (i in 1 until args.size) {
+                    val e = (args[i] as? Number)?.toFloat() ?: throw WrongTypeException(name, Type.Real::class.java, args[i])
+                    this[first.size + i - 1] = e
+                }
             }
             is Array<*> -> arrayOfNulls<Any?>(first.size + args.size - 1).apply {
                 for (i in 0 until first.size) { this[i] = first[i] }
-                System.arraycopy(args, 1, this, first.size, args.size - 1)
+                for (i in 1 until args.size)  { this[first.size + i - 1] = args[i] }
             }
             is Pair<*, *>  -> throw UnsupportedOperationException("$name: Pairs are not supported!")
             is MutablePair<*, *> -> throw UnsupportedOperationException("$name: Mutable Pairs are not supported!")
