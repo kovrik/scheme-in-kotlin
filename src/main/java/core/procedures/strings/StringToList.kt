dcff33109f120a08e6894188b24083349296482e
kovrik <kovrik0@gmail.com>
2018-05-12 21:44:35 +1200

    string->list: simplify

--- src/main/java/core/procedures/strings/StringToList.kt
+++ src/main/java/core/procedures/strings/StringToList.kt
@@ -3,9 +3,7 @@ package core.procedures.strings
 import core.procedures.AFn
 
 class StringToList : AFn<CharSequence?, List<Char?>>(name = "string->list", isPure = true, minArgs = 1, maxArgs = 1,
-                         mandatoryArgsTypes = arrayOf(CharSequence::class.java)) {
+                                                     mandatoryArgsTypes = arrayOf(CharSequence::class.java)) {
 
-    override operator fun invoke(arg: CharSequence?) = mutableListOf<Char>().apply {
-        arg!!.toString().toCharArray().forEach { add(it) }
-    }
+    override operator fun invoke(arg: CharSequence?) = arg!!.toList()
 }
