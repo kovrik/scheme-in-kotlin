e8993eab6d8ab0013581208dc3eea95017bbcdd5
kovrik <kovrik0@gmail.com>
2018-07-13 19:18:32 +1200

    call/cc: simplify arg type check

--- src/main/java/core/scm/specialforms/CallCC.kt
+++ src/main/java/core/scm/specialforms/CallCC.kt
@@ -2,17 +2,19 @@ package core.scm.specialforms
 
 import core.environment.Environment
 import core.Evaluator
+import core.exceptions.WrongTypeException
 import core.procedures.IFn
 import core.scm.CalledContinuation
 import core.scm.Continuation
-import core.scm.Type
 
 object CallCC : SpecialForm("call-with-current-continuation") {
 
     /* Actual call-with-current-continuation */
     override fun eval(form: List<Any?>, env: Environment, evaluator: Evaluator): Any? {
         val proc = evaluator.eval(form[1], env)
-        Type.assertType(name, proc, IFn::class.java)
+        if (proc !is IFn<*, *>) {
+            throw WrongTypeException(toString(), "Procedure", proc)
+        }
         val cont = Continuation()
         return try {
             /* Pass Continuation to the Procedure: (proc cont) */
