69702a0123333389669644d3c1fb8a80a64a908e
kovrik <kovrik0@gmail.com>
2018-07-24 18:25:48 +1200

    SImplify WeakPool

--- src/main/java/core/utils/WeakPool.kt
+++ src/main/java/core/utils/WeakPool.kt
@@ -3,13 +3,9 @@ package core.utils
 import java.lang.ref.WeakReference
 import java.util.WeakHashMap
 
-internal class WeakPool<T> {
-
-    private val pool = WeakHashMap<T, WeakReference<T>>()
+internal class WeakPool<T>(private val pool: WeakHashMap<T, WeakReference<T>> = WeakHashMap<T, WeakReference<T>>()) {
 
     operator fun get(obj: T) = pool[obj]?.get()
 
-    fun put(obj: T) {
-        pool[obj] = WeakReference(obj)
-    }
+    fun put(obj: T) = let { pool[obj] = WeakReference(obj) }
 }
