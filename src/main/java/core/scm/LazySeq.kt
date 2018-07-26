4e96cd5e3c8633b1b0c7a66ff3e962f41cd903cb
kovrik <kovrik0@gmail.com>
2018-07-26 17:08:37 +1200

    Fix LazySeq

--- src/main/java/core/scm/LazySeq.kt
+++ src/main/java/core/scm/LazySeq.kt
@@ -12,11 +12,12 @@ class LazySeq(private val form: List<Any?>, private val env: Environment, privat
     override fun iterator() = seq.cached().iterator()
 
     private val seq: Sequence<Any?> by lazy {
-        for (i in 1..form.size - 2) { evaluator.eval(form[i], env) }
         realized = true
         when (form.size) {
-            1    -> emptySequence<Nothing>()
-            else -> Utils.toSequence(evaluator.eval(form.last(), env))
+            1 -> emptySequence<Nothing>()
+            else -> (1..form.size - 2).forEach {
+                evaluator.eval(form[it], env)
+            }.let { Utils.toSequence(evaluator.eval(form.last(), env)) }
         }
     }
 }
