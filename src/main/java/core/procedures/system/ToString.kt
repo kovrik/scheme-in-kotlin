fc3fc7281a4d66f7ff8689bcaee6d93836067396
kovrik <kovrik0@gmail.com>
2018-07-26 17:13:05 +1200

    Simplify ToString

--- src/main/java/core/procedures/system/ToString.kt
+++ src/main/java/core/procedures/system/ToString.kt
@@ -8,7 +8,7 @@ open class ToString : AFn<Any?, CharSequence>(name = "->string", isPure = true) 
     override operator fun invoke(args: Array<out Any?>) = when (args.size) {
         0    -> ""
         1    -> str(args[0])
-        else -> StringBuilder().apply { args.forEach { append(str(it)) } }.toString()
+        else -> args.joinToString { str(it) }
     }
 
     private fun str(obj: Any?) = when (obj) {
