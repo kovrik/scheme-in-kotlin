782366094e4bc0ecc49c52810689321102dd15c5
kovrik <kovrik0@gmail.com>
2018-07-30 20:57:06 +1200

    Minor CharType fix

--- src/main/java/core/procedures/interop/CharType.kt
+++ src/main/java/core/procedures/interop/CharType.kt
@@ -7,7 +7,7 @@ import core.procedures.Arity.Exactly
 class CharType : AFn<Any?, Char>(name = "char", isPure = true, arity = Exactly(1)) {
 
     override operator fun invoke(arg: Any?) = when (arg) {
-        is Number -> arg.toInt().toChar()
+        is Number -> arg.toChar()
         is Char   -> arg
         else      -> throw WrongTypeException(name, "Character or Number", arg)
     }
