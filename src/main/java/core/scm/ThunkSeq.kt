23edb7394d83a93dbe3d1b4bef0fea3db369ac3c
kovrik <kovrik0@gmail.com>
2018-08-07 20:51:03 +1200

    Delete ThunkSeq

--- src/main/java/core/scm/ThunkSeq.kt
+++
@@ -1,5 +0,0 @@
-package core.scm
-
-import core.environment.Environment
-
-data class ThunkSeq<out T> @JvmOverloads constructor(val seq: Sequence<T>, val context: Environment? = null) : Sequence<T> by seq
