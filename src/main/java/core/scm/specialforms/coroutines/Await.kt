bd1a5e90f26b819c507469b771dfef2b6ebc6be7
kovrik <kovrik0@gmail.com>
2019-08-23 19:56:24 +1200

    Await

---
+++ src/main/java/core/scm/specialforms/coroutines/Await.kt
@@ -0,0 +1,25 @@
+package core.scm.specialforms.coroutines
+
+import core.Evaluator
+import core.Writer
+import core.environment.Environment
+import core.exceptions.IllegalSyntaxException
+import core.exceptions.WrongTypeException
+import core.scm.specialforms.SpecialForm
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.runBlocking
+
+/* Syntax:
+ * (await <deferred1>)
+ */
+object Await : SpecialForm("await") {
+
+    override fun eval(form: List<Any?>, env: Environment, evaluator: Evaluator) = runBlocking {
+        when (form.size == 2) {
+            true -> evaluator.eval(form[1], env).let {
+                it as? Deferred<Any?> ?: throw WrongTypeException(Await.name, "Coroutine", it)
+            }.await()
+            false -> throw IllegalSyntaxException(toString(), Writer.write(form))
+        }
+    }
+}
